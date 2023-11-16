9fa01d72ce3d1d3ce0bd5311b4078675d9fdce7a
Robert Nowotniak <rnowotniak@gmail.com>
2023-11-16 13:23:36 +0100

    Graceful Dropbox podman container stop added (TERM signal handler)

--- run.sh
+++ run.sh
@@ -10,8 +10,10 @@ nohup ~/.dropbox-dist/dropboxd 2>&1 &
 ps auxfw
 tail -f nohup.out &
 
+trap "echo 'TERM signal received'; dropbox.py stop" TERM
+
 while true; do
-	sleep 20
+	sleep 7
 	pidof dropbox &>/dev/null || { ps auxfw; echo "No dropbox process is running, terminating the container."; exit 1; }
 done
 	
